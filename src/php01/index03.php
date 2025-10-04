9088ef379b9d28648b1812ea9f3ce5341f3cd0f1
shiro <okano.03.remi@gmail.com>
2025-10-04 10:32:08 +0900

    2025/10/4

---
+++ src/php01/index03.php
@@ -0,0 +1,14 @@
+<?php
+$a=5;
+$b=10;
+echo "123","bbb";
+echo "aaa"."<br />";
+
+echo "This string spans
+multiple lines. The newlines will be
+output as well"."<br />";
+
+echo "This string spans\nmultiple lines. The newlines will be\noutput as well."."<br/>";
+
+echo ($a<$b)."<br />";
+echo $a<$b;
---
+++ src/php01/index08.php
@@ -0,0 +1,5 @@
+<?php
+$people = array('Taro', 'Jiro', 'Saburo');
+echo '<pre>';
+var_dump($people);
+echo '<pre>';
